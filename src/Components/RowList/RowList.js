d726e8a42bb83c18212a82be59c5e825e6bf0a9a
Fazil <mhdf088@gmail.com>
2023-07-11 23:19:49 +0530

    Movie lists row will be updated

--- src/Components/RowList/RowList.js
+++ src/Components/RowList/RowList.js
@@ -7,7 +7,6 @@ import { imageUrl } from '../../Constants/constants'
 function RowList(props) {
     const {url} = props
     const [movies,setMovies] = useState([])
-    const [titleName,setTitleName]=useState(null)
     const [youtubeUrl,setYoutubeUrl]=useState('')
     useEffect(()=>{
         instance.get(url).then((response)=>{
@@ -15,7 +14,7 @@ function RowList(props) {
         }).catch(err=>{
             console.log(err)
         })
-    },[])
+    },[]) // eslint-disable-line 
 
     function handleVideoPlay(id){
         instance.get(`movie/${id}/videos`).then((response)=>{
@@ -50,17 +49,11 @@ function RowList(props) {
         {
             movies.map((obj,k)=>{
                 return(
-                    <div className='imagesShow' 
-                    onMouseEnter={(e)=>{
-                        setTitleName(obj.id)
-                    }} onMouseLeave={()=>{
-                        setTitleName(null)
-                    }}
+                    <div className='imagesShow'
                      key={k}>
                         <img
                          onClick={()=>{handleVideoPlay(obj.id)}}
-                         className={props.isMedium ? (props.isSmall ? 'rowImgS3' : 'rowImgS2') : 'rowImgS1'} src={imageUrl+obj.backdrop_path} alt="Movie" />
-                        <h4 className='titleNames' >{titleName == obj.id ? obj.title : '' }</h4>
+                         className={props.isMedium ? (props.isSmall ? 'rowImgS3' : 'rowImgS2') : 'rowImgS1'} src={imageUrl+obj.poster_path} alt="Movie" />
                     </div>
                 )
             })
